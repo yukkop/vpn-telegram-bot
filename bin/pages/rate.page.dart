244e76cb59a062db76fe0303c0adba9da4eee3b2
yukkop <hectic.yukkop@gmail.com>
2023-02-07 14:05:12 +0100

    fix: rate buttons

--- bin/pages/rate.page.dart
+++ bin/pages/rate.page.dart
@@ -7,6 +7,7 @@ import 'package:vpn_telegram_bot/page-giga-mega-trash/text.hectic-tg.dart';
 import '../variables.dart';
 import 'main.page.dart';
 import 'pay/pay-choice.page.dart';
+import 'pay/pay.const.dart';
 
 late final rate = Page(
   text: Text.function((pageMessage, user) async {
@@ -16,27 +17,31 @@ late final rate = Page(
 );
 
 void rateKeyboard() {
-  rate.changeKeyboard(Keyboard.list([
-    [
-      Button.openPage(
-          text: dialogDataSource.getButtonText('rate', 'day', LayoutEnum.ru),
-          key: payFor1Day.getKey())
-    ],
-    [
-      Button.openPage(
-          text: dialogDataSource.getButtonText('rate', 'week', LayoutEnum.ru),
-          key: payFor1Week.getKey())
-    ],
-    [
-      Button.openPage(
-          text: dialogDataSource.getButtonText('rate', 'month', LayoutEnum.ru),
-          key: payFor1Month.getKey())
-    ],
-    [
-      Button.openPage(
-          text: dialogDataSource.getButtonText('rate', 'year', LayoutEnum.ru),
-          key: payFor1Year.getKey())
-    ],
-    [Button.openPage(text: 'Назад', key: mainMenuEdit.getKey())]
-  ]));
+  rate.changeKeyboard(Keyboard.function((pageMessage, user) async {
+    var message =
+        dialogDataSource.getButtonText('rate', 'choice', LayoutEnum.ru);
+    return [
+      [
+        Button.openPage(
+            text: Page.stringf(message, [textForDay, rubForDay.toString()]),
+            key: payFor1Day.getKey())
+      ],
+      [
+        Button.openPage(
+            text: Page.stringf(message, [textForWeek, rubForWeek.toString()]),
+            key: payFor1Week.getKey())
+      ],
+      [
+        Button.openPage(
+            text: Page.stringf(message, [textForMonth, rubForMonth.toString()]),
+            key: payFor1Month.getKey())
+      ],
+      [
+        Button.openPage(
+            text: Page.stringf(message, [textForYear, rubForYear.toString()]),
+            key: payFor1Year.getKey())
+      ],
+      [Button.openPage(text: 'Назад', key: mainMenuEdit.getKey())]
+    ];
+  }));
 }
