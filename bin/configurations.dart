410425d3f37de8afe829a8b8cd88a9d003548645
yukkop <hectic.yukkop@gmail.com>
2023-02-07 14:22:28 +0100

    fix: config

--- bin/configurations.dart
+++ bin/configurations.dart
@@ -4,6 +4,7 @@ class Configurations {
   static final botToken = lib_config.Configurations.config['botToken'];
   static final backendHost =
       '${lib_config.Configurations.config['backend']['host']}:${lib_config.Configurations.config['backend']['port']}';
-  static final iokassaToken = '${lib_config.Configurations.config['iokassa']}';
+  static final iokassaToken =
+      '${lib_config.Configurations.config['iokassaToken']}';
   static final botUrl = '${lib_config.Configurations.config['botUrl']}';
 }
